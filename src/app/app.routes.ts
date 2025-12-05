0cb83a561a29cad54555d026bc60c93031856b33
Bhatia <223068950@gehealthcare.com>
2025-12-06 00:15:51 +0530

    Added relevant pages and made login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/login/login';
+import { Signup } from './auth/signup/signup';
+import { AdminDashboard } from './admin/admin-dashboard/admin-dashboard';
+import { UserDashboard } from './user/user-dashboard/user-dashboard';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: Login, pathMatch: 'full' },
+    { path: 'signup', component: Signup, pathMatch: 'full' },
+    { path: 'admin', component: AdminDashboard, pathMatch: 'full' },
+    { path: 'user', component: UserDashboard, pathMatch: 'full' },
+
+
+];
